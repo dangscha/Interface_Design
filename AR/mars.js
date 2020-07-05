5acfa32c00587b301d0cd8b786e513065fc04089
Jan <jan.dangschat@gmx.de>
2020-07-05 21:53:53 +0200

    asd

--- AR/mars.js
+++ AR/mars.js
@@ -46,10 +46,10 @@ function createLights() {
 
     const ambientLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 3);
 
-    const mainLight = new THREE.DirectionalLight(0xffffff, 5);
+    const mainLight = new THREE.DirectionalLight(0xffffff, 1);
     mainLight.position.set(0, 10, 30);
 
-    const mainLight2 = new THREE.DirectionalLight(0xffffff, 5);
+    const mainLight2 = new THREE.DirectionalLight(0xffffff, 1);
     mainLight2.position.set(0, 10, -30);
 
     scene.add(ambientLight, mainLight, mainLight2);
